d8c9e857e74af7420d9457613d812dc5ac39d74a
Christian Prentice <purin213@hotmail.com>
2025-01-06 23:18:26 +0900

    check

--- Tools/fishrc/config.fish
+++ Tools/fishrc/config.fish
@@ -2,8 +2,9 @@ if status is-interactive
 	# Commands to run in interactive sessions can go here
 
 	#ALIAS
-	alias tree='eza -ls ext --icons --tree'
-	alias ls='eza -ls ext --icons'
+	alias tree='eza -ls type --icons --tree'
+	alias ls='eza -s type --icons'
+	alias ll='eza -ls type --icons'
     alias vim='nvim'
     alias rmatrix='rusty-rain -C green -H 255,255,255 -c alphalow -s'
     alias zj='zellij'
